021417e4e9103c826b0370d58f77a81a139fb579
Diesel <belijivan9@gmail.com>
2025-10-02 13:54:32 +0300

    Add custom fix (5-15 lines) to calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,6 +5,10 @@ int Calculator::Add (double a, double b)
     return a + b;
 }
 
+// My custom changes for lab work
+// Added comments and minor improvements
+// This part of the code was modified manually
+
 int Calculator::Sub (double a, double b)
 
 {
